3a866f34384edd73a6ea0c8563415dcc58c588be
yyflyons <yyflyons@gmail.com>
2012-07-13 12:49:40 +0800

    增加几个公用类

---
+++ antelopeframe/src/main/java/com/ifunshow/antelopeframe/core/web/interceptor/LogInterceptor.java
@@ -0,0 +1,7 @@
+package com.ifunshow.antelopeframe.core.web.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LogInterceptor extends HandlerInterceptorAdapter {
+	
+}

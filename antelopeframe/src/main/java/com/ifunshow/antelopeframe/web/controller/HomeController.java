933431f690d05942f7feb5eccc4169beac7f24a4
yyflyons <yyflyons@gmail.com>
2012-07-13 12:52:46 +0800

    增加jquery和home类修改

--- antelopeframe/src/main/java/com/ifunshow/antelopeframe/web/controller/HomeController.java
+++ antelopeframe/src/main/java/com/ifunshow/antelopeframe/web/controller/HomeController.java
@@ -1,7 +1,12 @@
 package com.ifunshow.antelopeframe.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ifunshow.antelopeframe.core.web.SpringContextHolder;
 
 /**
  * 欢迎页面
@@ -14,4 +19,13 @@ public class HomeController {
 	public String home(){
 		return "home";
 	}
+	
+	@RequestMapping("/home/springBean/{beanName}")
+	@ResponseBody
+	public Model queryBeanByName(Model model, @PathVariable String beanName){
+		model.addAttribute(beanName, SpringContextHolder.getBean(beanName));
+		return model;
+	}
+	
+	
 }
